497ea00f23db5c53b2c8cf9c9d844f17dec05fe3
Julien Dargelos <julien.dargelos@me.com>
2020-10-13 17:26:20 +0200

    Change api endpoint, add cors

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require('express')
+const cors = require('cors')
 const scraper = require('./scraper')
 const app = express();
 
-//Home page
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-//API route
-app.get('/api/search', (req, res) => {
-    scraper.youtube(req.query.q, req.query.page)
-        .then(x => res.json(x))
-        .catch(e => res.send(e));
-});
+app.get('/', cors({
+  origin: (process.env.ALLOWED_ORIGINS || '')
+    .split(',')
+    .map(origin => origin.trim())
+}), (req, res) => {
+  scraper.youtube(req.query.q, req.query.page)
+    .then(x => res.json(x))
+    .catch(e => res.send(e))
+})
 
 app.listen(process.env.PORT || 8080, function () {
-  console.log('Listening on port 8080');
-});
+  console.log('Listening on port 8080')
+})
 
-module.exports = app;
+module.exports = app
